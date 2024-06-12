a19bdca1fab660aa661c60ddb3dc2126943ace67
partapparam <paramsingh1961@gmail.com>
2024-06-11 21:57:41 -0700

    llm prompts setup

---
+++ src/researcher/master/prompts.py
@@ -0,0 +1,12 @@
+def generate_sub_query_prompt(
+    queries: list[str],
+    parent_query: str,
+    agent_role_prompt: str,
+):
+    return f"""
+        your role: {agent_role_prompt}
+        Using the following list of queries: "{queries}", and generate one additional query that can be used to search for information online. \n
+        The additional query should be relevant to the main query ({parent_query}) and should be structured in a logical and coherent manner. \n
+        The output should be brief and not contain any additional text, explanations, or data structures. \n
+        Returns: str: The search query
+    """
