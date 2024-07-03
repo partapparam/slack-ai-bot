596798363a0e1810cfb65fce9d19b6a6d06009e2
partapparam <paramsingh1961@gmail.com>
2024-07-02 21:22:51 -0700

    create the research class

---
+++ src/researcher/master/agent.py
@@ -0,0 +1,13 @@
+import asyncio
+import time
+from researcher.master.functions import *
+from datetime import datetime
+from typing import Union
+
+class Researcher:
+    """
+        Our Research Agent
+    """
+    def __init(self):
+        pass
+    
