ff7bdc14365d0bf9ba0b4b066b72035dd80c730c
partapparam <paramsingh1961@gmail.com>
2024-07-08 20:26:44 -0700

    agent search and get context function

--- src/researcher/master/agent.py
+++ src/researcher/master/agent.py
@@ -41,22 +41,42 @@ class Researcher:
 
     async def conduct_research(self):
         """
-        Runs the GPT Researcher to conduct research
+        Runs the Researcher to conduct research
         """
         print(f"Research started on '{self.query}")
         # Generate Agent
         if not (self.agent and self.role):
             self.agent, self.role = await choose_agent(query=self.query, self.cfg)
-        ##
-        # Get search by using the URLs provided
-        ##
-        # If specified, the researcher will use the given urls as the context for the research.
-        # if self.source_urls:
-        #     self.context = await self.get_context_by_urls(self.source_urls)
-        # else:
-        #     self.context = await self.get_context_by_search(self.query)
-        
-        #
-        #  Setup to search based on query
         self.context = await self.get_context_by_search(self.query)
         return self
+
+
+    async def get_context_by_search(self, query):
+        """
+           Generates the context for the research query.
+           Creates subqueries using decomposition.
+           Searches and scrapes for each query
+        Returns:
+            context: List of context
+        """
+        context = []
+
+        # error handling in the event decomposition fails
+        try:
+            # Generate Sub-Queries including original query
+            self.subqueries = await get_sub_queries(
+                query, self.role, self.cfg, self.parent_query, self.report_type
+            )
+        except Exception as e:
+            print(f"Error in get_sub_queries: {e}")
+            print(f"Traceback: {traceback.format_exc()}")
+            self.subqueries = [query]
+
+        print(f"I will conduct my research based on the following queries: {self.subqueries}...")
+
+        # Using asyncio.gather to process the sub_queries asynchronously
+        context = await asyncio.gather(
+            *[self.process_sub_query(sub_query) for sub_query in self.sub_queries]
+        )
+
+        return context
