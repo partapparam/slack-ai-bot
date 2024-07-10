7708fd01422b76335ca2effab507ccbb613e51ec
partapparam <paramsingh1961@gmail.com>
2024-07-09 21:24:39 -0700

    body base model

--- backend/server.py
+++ backend/server.py
@@ -7,3 +7,16 @@ import json
 import datetime
 
 app = FastAPI()
+
+class Body(BaseModel):
+    """Represents the body of a POST request
+
+    Attributes:
+        query(str): The query to search
+        request_id(Union[str, None]): The unique identifier for the research request.
+        max_resources(Union[int, None]): The number of scraped resources to return - Optional
+    """
+
+    query: str
+    request_id: Union[str, None] = None
+    max_resources: Union[int, None] = None
