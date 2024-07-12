d5ae10a88258b554e3ccc83238a43efaed42c9a5
partapparam <paramsingh1961@gmail.com>
2024-07-11 21:29:06 -0700

    add slack commands

--- backend/server.py
+++ backend/server.py
@@ -61,23 +61,6 @@ def event_test(body, say, logger):
     logger.info(body)
     say("What's up?")
 
-@app.command(command='/modify')
-def handle_modify_bot(ack: Ack, body: Dict[str, Any], respond: Respond, context, client, payload, command) -> None:
-    """
-    Handle the /modify-bot command
-    This function modifies the Bots scope and access for questions
-    """
-    ack()
-    channel_id = body['channel_id']
-    trigger_id = body['trigger_id']
-    print(channel_id, trigger_id)
-     # Load modify_bot_template.json payload
-    with open(f'../src/templates/file_upload_template.json', 'r') as f:
-        view = json.load(f)
-    respond(f"{command['text']}")
-    client.views_open(trigger_id=trigger_id, view=view)
-
-
 api = FastAPI()
 
 @api.post("/slack/events")
