6c870ab246797e5946c60816eb95e0257bdcb368
partapparam <paramsingh1961@gmail.com>
2024-07-22 22:09:06 -0700

    scraper run functino

--- src/researcher/scraper/scraper.py
+++ src/researcher/scraper/scraper.py
@@ -23,4 +23,23 @@ class Scraper:
         self.scraper = scraper
         self.query = query
 
+    def run(self) -> list:
+        """
+        Extracts the content from the URLs using the specified scraper,
+        creates a Source object for each URL, and returns a list of Source objects
+        with non-empty parsed text.
+        """
+        # partial_extract = partial(self.extract_data_from_link, session=self.session)
+        # with ThreadPoolExecutor(max_workers=20) as executor:
+        #     contents = executor.map(partial_extract, self.urls)
+        # res = [content for content in contents if content["raw_content"] is not None]
+        # return res
+
+        partial_extract = partial(self.extract_data_from_link, session=self.session)
+        with ThreadPoolExecutor(max_workers=20) as executor:
+            sources = executor.map(partial_extract, self.urls)
+
+        # The return value from extract_data_from_link is either source or None
+        return [source for source in sources if source is not None]
+        # return sources
     
