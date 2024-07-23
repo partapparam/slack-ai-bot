17205a9c80fdf91d4ffaac7c442cadd795bb50fd
partapparam <paramsingh1961@gmail.com>
2024-07-22 22:02:23 -0700

    update imports

--- src/researcher/scraper/scraper.py
+++ src/researcher/scraper/scraper.py
@@ -3,7 +3,7 @@ from functools import partial
 
 import requests
 
-from src.researcher.scraper.beautiful_soup.beautiful_soup import (
+from src.researcher.scraper.beautiful_soup import (
     BeautifulSoupScraper,
 )
 
