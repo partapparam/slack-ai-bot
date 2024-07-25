b5c4a180ead9266caefd8a3cd60fa6341db57c31
partapparam <paramsingh1961@gmail.com>
2024-07-24 17:24:53 -0700

    researcher imports

--- src/researcher/__init__.py
+++ src/researcher/__init__.py
@@ -1,6 +1,5 @@
 from .master import Researcher
 from .config import Config
 from .context import ContextCompressor
-from .scraper import Scraper
 
-__all__ = ['Researcher', 'Config', 'ContextCompressor', 'Scraper']
+__all__ = ['Researcher', 'Config', 'ContextCompressor']
